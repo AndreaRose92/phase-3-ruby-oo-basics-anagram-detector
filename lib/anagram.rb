799807d4f8aac7c09922573233eba10455f63fe3
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-08 13:54:11 -0700

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+
+class Anagram
+
+    attr_reader :word
+    # attr_accessor :new_word
+
+    array = %w[]
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter { |new_word|
+            @word.chars.sort == new_word.chars.sort
+    }
+        
+    end
+
+end
